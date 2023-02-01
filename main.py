f433524d9035bfae43e1559da4f38cab2f990b1b
Christina P <reetagreen@users.noreply.github.com>
2023-02-01 18:45:05 +0000

    further improvement

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import random
 
-def game_e():
-  player = input("Enter your choice (rock,paper,scissor,lizard): ")
+def game():
+  #player = input("Enter your choice (rock,paper,scissor,lizard): ")
+  player="scissors"
   choices = ["rock","paper","scissor","lizard"]
   computer = random.choice(choices)
   choiceDict = {"Player":player,"Computer":computer}
-  return choiceDict
+  return print(choiceDict)
+
+game()
+
+def check_win(player,computer):
+  print(f"***you chose {player}***")
+  print(f"**computer chose {computer}**")
+  if player==computer:
+    return "It's a tie!"
+
+age=25
+print(f"Jim is {age} years old")
+
+"""
+a=3
+b=1
+if a>b: # ==  != <= >=
+  print ("a is bigger")
+else:
+  print ("b is bigger")
+"""
